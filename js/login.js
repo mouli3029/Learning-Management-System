0b823a52a0f8a2a5a9ad271689381d92f319292c
Mouli <venkatmouli823@gmail.com>
2021-02-19 00:25:05 +0530

    Added class diagrams

--- js/login.js
+++ js/login.js
@@ -1,5 +1,4 @@
 var emailInput = document.querySelector('.card-input-email')
 var password  = document.querySelector('.card-input-password')
-
 console.log(emailInput.value);
 console.log(password.value);
